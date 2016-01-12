831b5857e6adf7e878fcd0b3695088421b7a7f83
dlwhitehurst <dlwhitehurst@me.com>
2016-01-12 08:36:37 -0500

    CWRC-1 fix, and add target ignore

--- src/main/java/org/ciwise/modules/ciwiserest/config/ConnectorConfig.java
+++ src/main/java/org/ciwise/modules/ciwiserest/config/ConnectorConfig.java
@@ -5,4 +5,14 @@ import org.mule.api.annotations.components.Configuration;
 @Configuration(friendlyName = "Configuration")
 public class ConnectorConfig {
 
+	private final String reply = "Hello";
+	private final String greeting = "How are you?";
+
+	public String getReply() {
+		return reply;
+	}
+	public String getGreeting() {
+		return greeting;
+	}
+	
 }
