9d6851fa114482d4e4b4cdc93a19ebaec8f2bd05
dlwhitehurst <dlwhitehurst@me.com>
2016-01-14 16:11:11 -0500

    CWRC-1 fixed build issue and API call to get Accounts

--- src/main/java/org/ciwise/modules/ciwiserest/CIWiseRESTConnector.java
+++ src/main/java/org/ciwise/modules/ciwiserest/CIWiseRESTConnector.java
@@ -1,9 +1,14 @@
 package org.ciwise.modules.ciwiserest;
 
+import org.ciwise.modules.ciwiserest.client.RestClient;
+import org.ciwise.modules.ciwiserest.config.ConnectorConfig;
+import org.ciwise.modules.ciwiserest.entity.Account;
+import org.ciwise.modules.ciwiserest.exception.CIWiseRESTConnectorException;
+import org.ciwise.modules.ciwiserest.exception.CIWiseRESTConnectorTokenExpiredException;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Processor;
-import org.ciwise.modules.ciwiserest.config.ConnectorConfig;
+import org.mule.api.annotations.lifecycle.Start;
 
 @Connector(name="ci-wise-rest", friendlyName="CIWiseREST")
 public class CIWiseRESTConnector {
@@ -11,6 +16,13 @@ public class CIWiseRESTConnector {
     @Config
     ConnectorConfig config;
 
+    private String token;
+	private RestClient client;
+
+    @Start
+    public void init() {
+        setClient(new RestClient(this));
+    }    
     /**
      * Custom processor
      *
@@ -25,6 +37,25 @@ public class CIWiseRESTConnector {
         return config.getGreeting() + " " + friend + ". " + config.getReply();
     }
 
+    @Processor(friendlyName="Get X-Auth Token")
+    public String getXAuthToken() throws CIWiseRESTConnectorException, CIWiseRESTConnectorTokenExpiredException {
+        org.ciwise.modules.ciwiserest.entity.Token token = getClient().getXAuthToken();
+    	return "The token is: " + (String) token.getToken();
+    }
+ 
+    @Processor(friendlyName="Get Accounts")
+    public String getAccounts() throws CIWiseRESTConnectorException, CIWiseRESTConnectorTokenExpiredException {
+    	Account[] array = getClient().getAccounts();
+    	String retStr = "";
+    	for(int i=0; i < array.length; i++)
+    	{
+    	    retStr = retStr + array[i].getId() + "," + array[i].getName() + "," + array[i].getAcctno() + "\n";
+    	    
+    	}
+    	
+    	return retStr;
+    }
+    
     public ConnectorConfig getConfig() {
         return config;
     }
@@ -33,4 +64,29 @@ public class CIWiseRESTConnector {
         this.config = config;
     }
 
+	/**
+	 * @return the token
+	 */
+	public String getToken() {
+		return token;
+	}
+
+	/**
+	 * @param token the token to set
+	 */
+	public void setToken(String token) {
+		this.token = token;
+	}
+	/**
+	 * @return the client
+	 */
+	public RestClient getClient() {
+		return client;
+	}
+	/**
+	 * @param client the client to set
+	 */
+	public void setClient(RestClient client) {
+		this.client = client;
+	}
 }
--- src/main/java/org/ciwise/modules/ciwiserest/config/ConnectorConfig.java
+++ src/main/java/org/ciwise/modules/ciwiserest/config/ConnectorConfig.java
@@ -1,18 +1,42 @@
 package org.ciwise.modules.ciwiserest.config;
 
+import org.mule.api.annotations.Configurable;
 import org.mule.api.annotations.components.Configuration;
+import org.mule.api.annotations.param.Default;
 
 @Configuration(friendlyName = "Configuration")
 public class ConnectorConfig {
 
+
+    private final String apiUrl = "http://localhost:8080";
+	
 	private final String greeting = "Hello";
 	private final String reply = "How are you?";
-
+	private final String username = "user";
+	private final String password = "user";
+	
 	public String getReply() {
 		return reply;
 	}
 	public String getGreeting() {
 		return greeting;
 	}
-	
+	/**
+	 * @return the apiUrl
+	 */
+	public String getApiUrl() {
+		return apiUrl;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
 }
